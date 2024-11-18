a1a782e20596da693f9d33b3a65970b9d53e275f
josemiguel736 <robertz@youremail.com>
2024-11-18 20:32:08 +0100

    vistas y conexion db

---
+++ models/apiPop-model.js
@@ -0,0 +1,17 @@
+//API
+
+export async function getProducts() {
+    try {
+        //conexion con la api
+        const response = await fetch("http://localhost:8000/api/products")
+        
+        if(!response.ok){
+            throw new Error("El recurso no existe o está inaccesible")
+        }
+        const products = await response.json()
+        return products
+
+    } catch (error) {
+        throw new Error("El servidor no responde, por favor vuelva a intentarlo más tarde")        
+    }    
+}
---
+++ models/apiPop-views.js
@@ -0,0 +1,30 @@
+//html que vamos a mostrar a nuestro usuario
+export const buildProduct= (product)=>{
+    //creo un nodo con mi html dentro
+    const newProduct = document.createElement('div')
+    if (product.image != ""){
+        newProduct.innerHTML=`
+        <div class="product">
+        <img src=${product.image} alt=${product.name}>
+        <div>${product.name}</div>
+        <div>${product.cost} €</div>
+        <div>${product.status}</div>
+        </div> `
+    }else{
+        newProduct.innerHTML=`
+        <div class="product">
+        <div>${product.name}</div>
+        <div>${product.cost} €</div>
+        <div>${product.status}</div>
+        </div> `
+    }   
+    return newProduct
+}
+
+export const buildNoProducts= ()=>{
+    //creo un nodo con mi html dentro
+    const newProduct = document.createElement('span')
+    newProduct.innerHTML="No hay productos"
+
+    return newProduct
+}
